fcb4e9de0d1b78b1d8010de2eaf8856717acab23
Rohit Singh <kailash.jc.rs@gmail.com>
2023-03-17 14:58:39 +0530

    mens page updated

--- src/pages/Mens.jsx
+++ src/pages/Mens.jsx
@@ -20,16 +20,16 @@ const Mens = () => {
     }, [])
 
     return (
-        <Box w={{ lg: '75%', md: '100%', sm: '100%' }} margin='auto'>
+        <Box w={{ base: '98%', lg: '75%', md: '100%', sm: '100%' }} margin='auto'>
 
-            <Flex h={{ lg: '71vh', md: '46vh', sm: '46vh' }} pb={10} justifyContent='flex-end' direction='column' alignItems='center' w='100%' backgroundPosition='center' backgroundSize='cover' backgroundImage={'https://img.mytheresa.com/media/static/raw/cms/l/SM_Monetate_Images/14_ActivityBoxes/Fixes/MonclerCover/BIG_DESKTOP_1X_20220928100157.jpg'}>
+            <Flex h={{ lg: '72vh', md: '60vh', sm: '46vh', base: '35vh' }} pb={10} justifyContent='flex-end' direction='column' alignItems='center' w='100%' backgroundPosition='center' backgroundSize='cover' backgroundImage={'https://img.mytheresa.com/media/static/raw/cms/l/SM_Monetate_Images/14_ActivityBoxes/Fixes/MonclerCover/BIG_DESKTOP_1X_20220928100157.jpg'}>
                 <Flex width='100%' justifyContent='center' alignItems='center' direction='column'>
                     <Stack spacing={2}>
                         <Text color='white' textAlign='center' letterSpacing={{ lg: '1', md: '0' }}>UNEARTH YOUR AUTHENTIC STYLE</Text>
                         <Text color='white' fontSize={{ lg: '55', md: '35', sm: '33' }} textAlign='center'>Pre-Fall 2022</Text>
                     </Stack>
                     <Link to='/menproducts'>
-                        <Button colorScheme='white' bgColor='white' maxW={{ lg: '6vw', md: '10vw', sm: '9vw' }} h={8} fontSize={13} borderRadius='none' color='blackAlpha.800' mt='1.2rem' fontWeight='none' w='20vw'>SHOP NOW</Button>
+                        <Button colorScheme='white' bgColor='white' maxW={{ lg: '6vw', md: '14vw', sm: '13vw', base: '22vw' }} h={8} fontSize={13} borderRadius='none' color='blackAlpha.800' mt='1.2rem' fontWeight='none'>SHOP NOW</Button>
                     </Link>
                 </Flex>
             </Flex>
@@ -39,57 +39,57 @@ const Mens = () => {
                 <ProductCarousel products={data} category='men' />
                 <Center>
                     <Link to='/menproducts'>
-                        <Button colorScheme='blackAlpha' maxW={{ lg: '6vw', md: '10vw', sm: '9vw' }} h={8} fontSize={13} bgColor='blackAlpha.800' borderRadius='none' color='white' mt='1.6rem' w='10vw'>VIEW ALL</Button>
+                        <Button colorScheme='blackAlpha' maxW={['20vw', '13vw', '10vw', '6vw']} h={8} fontSize={13} bgColor='blackAlpha.800' borderRadius='none' color='white' mt='1.6rem'>VIEW ALL</Button>
                     </Link>
                 </Center>
             </Box>
 
-            <SimpleGrid columns={{ lg: '2', md: '1', sm: '1' }} h='70vh' w='100%'>
+            <SimpleGrid columns={{ lg: '2', md: '1', sm: '1', base: '1' }} h={['58vh', '60vh', '70vh', '70vh']} w='100%'>
                 <Flex justifyContent='center' alignItems='center' color='white' direction='column' w='100%' backgroundColor='black'>
                     <Stack spacing={4}>
-                        <Stack spacing={-3} fontSize='3.5rem'>
+                        <Stack pl={['1rem', '1rem', '0', '0']} spacing={-3} fontSize={['2.3rem', '2.4rem', '3.5rem', '3.5rem']}>
                             <Text>Outerwear</Text>
                             <Text>remodel</Text>
                         </Stack>
-                        <Stack spacing={7}>
+                        <Stack pl={['1rem', '1rem', '0', '0']} spacing={['2', '2', '7', '7']}>
                             <Box>
                                 <Text fontSize={16}>Add a fresh coat to your cold-weather edit with tailored styles</Text>
                                 <Text fontSize={16}>taking bold shape</Text>
                             </Box>
                             <Link to='/menproducts'>
-                                <Button colorScheme='gray' bgColor='white' maxW={{ lg: '6vw', md: '10vw', sm: '9vw' }} h={8} fontSize={13} borderRadius='none' color='blackAlpha.800' mt='1.2rem' fontWeight='none' w='25vw'>SHOP NOW</Button>
+                                <Button colorScheme='blackAlpha' maxW={{ lg: '6vw', md: '10vw', sm: '13vw', base: '20vw' }} h={8} fontSize={13} bgColor='white' borderRadius='none' color='black' mt='1.6rem' mb={['1.3rem', '1.3rem', 'none', 'none']}>VIEW ALL</Button>
                             </Link>
                         </Stack>
                     </Stack>
                 </Flex>
-                <Box display={{ lg: 'inline-block', md: 'none', sm: 'none' }} backgroundPosition='center' backgroundSize='cover' backgroundImage='https://img.mytheresa.com/media/static/raw/cms/l/MW_HP_2022_CW39/BIG_2_3X_20220926111306.jpg?imwidth=1180&imdensity=1' >
+                <Box display={{ lg: 'inline-block', md: 'none', sm: 'none', base: 'none' }} backgroundPosition='center' backgroundSize='cover' backgroundImage='https://img.mytheresa.com/media/static/raw/cms/l/MW_HP_2022_CW39/BIG_2_3X_20220926111306.jpg?imwidth=1180&imdensity=1' >
                 </Box>
             </SimpleGrid>
 
             <Box mt='2rem' mb={12} pt={5}>
-                <MenCarousel1 products={data} category='men' />
+                <MenCarousel2 products={data} category='men' />
                 <Center>
                     <Link to='/menproducts'>
-                        <Button colorScheme='blackAlpha' maxW={{ lg: '6vw', md: '10vw', sm: '9vw' }} h={8} fontSize={13} bgColor='blackAlpha.800' borderRadius='none' color='white' mt='1.6rem' w='10vw'>VIEW ALL</Button>
+                        <Button colorScheme='blackAlpha' maxW={{ lg: '6vw', md: '10vw', sm: '13vw', base: '20vw' }} h={8} fontSize={13} bgColor='blackAlpha.800' borderRadius='none' color='white' mt='1.6rem' mb={['1.3rem', '1.3rem', 'none', 'none']}>VIEW ALL</Button>
                     </Link>
                 </Center>
             </Box>
 
-            <SimpleGrid columns={{ lg: '2', md: '1', sm: '1' }} h='70vh' w='100%'>
-                <Box display={{ lg: 'inline-block', md: 'none', sm: 'none' }} backgroundPosition='center' backgroundSize='cover' backgroundImage='https://img.mytheresa.com/media/static/raw/cms/l/MW_HP_2022_CW39/BIG_3_2X_20220926111307.jpg?imwidth=1180&imdensity=1' >
+            <SimpleGrid columns={{ lg: '2', md: '1', sm: '1', base: '1' }} h={['50vh', '60vh', '70vh', '70vh']} w='100%'>
+                <Box display={{ lg: 'inline-block', md: 'none', sm: 'none', base: 'none' }} backgroundPosition='center' backgroundSize='cover' backgroundImage='https://img.mytheresa.com/media/static/raw/cms/l/MW_HP_2022_CW39/BIG_3_2X_20220926111307.jpg?imwidth=1180&imdensity=1' >
                 </Box>
                 <Flex justifyContent='center' alignItems='center' color='white' direction='column' w='100%' backgroundColor='black'>
-                    <Stack spacing={4}>
-                        <Stack fontSize='3.5rem'>
+                    <Stack spacing={['3', '3', '3', '3']}>
+                        <Stack pl={['1rem', '1rem', '0', '0']} fontSize={['2.8rem', '2.4rem', '3.5rem', '3.5rem']}>
                             <Text>Next in neutrals</Text>
                         </Stack>
-                        <Stack spacing={7}>
+                        <Stack pl={['1rem', '1rem', '0', '0']} spacing={['2', '2', '7', '7']}>
                             <Box>
                                 <Text fontSize={16}>Experiment with a cool-toned palatette of neutrals to evoke</Text>
                                 <Text fontSize={16}>composure in any weather</Text>
                             </Box>
                             <Link to='/menproducts'>
-                                <Button colorScheme='gray' bgColor='white' maxW={{ lg: '6vw', md: '10vw', sm: '9vw' }} h={8} fontSize={13} borderRadius='none' color='blackAlpha.800' mt='1.2rem' fontWeight='none' w='25vw'>SHOP NOW</Button>
+                                <Button colorScheme='gray' bgColor='white' maxW={{ lg: '6vw', md: '10vw', sm: '14vw', base: '21vw' }} h={8} fontSize={13} borderRadius='none' color='blackAlpha.800' mt='1.2rem' fontWeight='none' w='25vw'>SHOP NOW</Button>
                             </Link>
                         </Stack>
                     </Stack>
@@ -100,7 +100,7 @@ const Mens = () => {
                 <MenCarousel2 products={data} category='men' />
                 <Center>
                     <Link to='/menproducts'>
-                        <Button colorScheme='blackAlpha' maxW={{ lg: '6vw', md: '10vw', sm: '9vw' }} h={8} fontSize={13} bgColor='blackAlpha.800' borderRadius='none' color='white' mt='1.6rem' w='10vw'>VIEW ALL</Button>
+                        <Button colorScheme='blackAlpha' maxW={{ lg: '6vw', md: '10vw', sm: '13vw', base: '20vw' }} h={8} fontSize={13} bgColor='blackAlpha.800' borderRadius='none' color='white' mt='1.6rem' mb={['1.3rem', '1.3rem', 'none', 'none']}>VIEW ALL</Button>
                     </Link>
                 </Center>
             </Box>
